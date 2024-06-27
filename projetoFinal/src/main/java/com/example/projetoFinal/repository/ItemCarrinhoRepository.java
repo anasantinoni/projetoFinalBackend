0456530d0e3e7ebd211ddcc19d43f22469da26b3
Ana Júlia Santinoni <129432695+anasantinoni@users.noreply.github.com>
2024-06-27 01:37:05 -0300

    Delete projetoFinal directory

--- projetoFinal/src/main/java/com/example/projetoFinal/repository/ItemCarrinhoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projetoFinal.repository;
-
-import com.example.projetoFinal.model.ItemCarrinho;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ItemCarrinhoRepository extends JpaRepository<ItemCarrinho, Long> {
-}
